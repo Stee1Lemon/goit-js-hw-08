01e2c3cfd75fe67f30d34cd78cd17359511d90d8
Stee1Lemon <121914099+Stee1Lemon@users.noreply.github.com>
2023-07-13 20:19:53 +0100

    task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  galleryEl: document.querySelector('.gallery'),
+};
+
+createFinalGalleryByReduce(galleryItems);
+
+function createFinalGalleryByReduce(arrayImages) {
+  const finalGallery = arrayImages.reduce(
+    (accumulator, item) =>
+      (accumulator += `<li class="gallery__item">
+          <a class="gallery__link" href="${item.original}">
+            <img class="gallery__image" src="${item.preview}" alt="${item.description}"/>
+          </a>
+        </li>`),
+    ''
+  );
+
+  refs.galleryEl.insertAdjacentHTML('beforeend', finalGallery);
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+const liElement = document.querySelectorAll('.gallery, li');
+
+liElement.forEach(item => {
+  item.style.listStyleType = 'none';
+  console.log();
+});
